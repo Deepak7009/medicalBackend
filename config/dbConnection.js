3880c49a4818c4ca2de8cf740e2dae956de9284f
Deepak7009 <deepak7009verma@gmail.com>
2024-04-05 12:30:07 +0530

    envFile

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,16 +1,12 @@
 const mongoose = require("mongoose");
-
 const conn = async () => {
-    try {
-        const connect = await mongoose.connect("mongodb://127.0.0.1:27017/mediData")
-        console.log("DB Connect",
-            connect.connection.host,
-        );
-    }
-    catch (err) {
-        console.error("DB connection error:", err);
-        process.exit(1);
-    }
-}
+  try {
+    const connect = await mongoose.connect(process.env.MONGODB_URI);
+    console.log("DB Connect", connect.connection.host);
+  } catch (err) {
+    console.error("DB connection error:", err);
+    process.exit(1);
+  }
+};
 
 module.exports = conn;
